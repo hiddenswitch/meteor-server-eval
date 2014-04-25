f8fd8380a82ba6b872628dc415bb879d4deb8afe
Benjamin S. Berman <benjamin.s.berman@gmail.com>
2014-04-25 14:05:01 -0700

    Maybe it's an ordering issue

--- package.js
+++ package.js
@@ -6,12 +6,11 @@ Package.on_use(function(api) {
   // Support user id and facebook id
   api.use('standard-app-packages', ['client', 'server']);
   api.use('accounts-facebook', ['client', 'server']);
-
-  api.add_files('server/result_format.js', 'server');
+  api.add_files('client/client.js', 'client')
   api.add_files('server/eval.js', 'server');
+  api.add_files('server/result_format.js', 'server');
   api.add_files('server/helpers.js', 'server');
   api.add_files('server/git_helpers.js', 'server');
-  api.add_files('client/client.js', 'client')
-
+  
   api.export('ServerEval');
 });
