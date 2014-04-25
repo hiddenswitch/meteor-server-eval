30189b531e3dfd298ee3be6fe68f5fd075617e6f
Benjamin S. Berman <benjamin.s.berman@gmail.com>
2014-04-25 13:15:45 -0700

    Fixing api export

--- package.js
+++ package.js
@@ -8,11 +8,11 @@ Package.on_use(function(api) {
   // Support user id and facebook id
   api.use(['accounts-password', 'accounts-facebook'], 'server');
 
-  api.export('ServerEval');
-
   api.add_files('result_format.js', 'server');
   api.add_files('eval.js', ['client', 'server']);
 
   api.add_files('helpers.js', 'server');
   api.add_files('git_helpers.js', 'server');
+
+  api.export('ServerEval', ['server', 'client']);
 });
